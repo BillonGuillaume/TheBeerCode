f490057f142f18b9f365b6e0184d123fab200fa6
Al0kar <billon.guillaume@gmail.com>
2013-11-21 15:51:47 +0100

    Personne implémente maintenant Parcelable

--- TheBeerCode/src/com/fjbg/thebeercode/model/Personne.java
+++ TheBeerCode/src/com/fjbg/thebeercode/model/Personne.java
@@ -1,7 +1,10 @@
 package com.fjbg.thebeercode.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Personne {
+
+public class Personne implements Parcelable{
 	protected int idPersonne;
 	protected String login;
 	protected String mdp;
@@ -51,5 +54,37 @@ public class Personne {
 	public void setPays(String pays) {
 		this.pays = pays;
 	}
-	
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeInt(idPersonne);
+		dest.writeString(login);
+		dest.writeString(mdp);
+		dest.writeString(mail);
+		dest.writeString(pays);
+	}
+
+	public static final Parcelable.Creator<Personne> CREATOR = new Parcelable.Creator<Personne>() {
+		@Override
+		public Personne createFromParcel(Parcel source) {
+			return new Personne(source);
+		}
+		@Override
+		public Personne[] newArray(int size) {
+			return new Personne[size];
+		}
+	};
+	public Personne(Parcel in) {
+		idPersonne = in.readInt();
+		login = in.readString();
+		mdp = in.readString();
+		mail = in.readString();
+		pays=in.readString();
+	}
+
 }
