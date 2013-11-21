033636cba6e5494a2bee888ef163a6a9cce357f6
Al0kar <billon.guillaume@gmail.com>
2013-11-21 11:47:54 +0100

    Mis en place des méthodes de MainActivity pour le mode non connecté

--- TheBeerCode/src/com/fjbg/thebeercode/MainActivity.java
+++ TheBeerCode/src/com/fjbg/thebeercode/MainActivity.java
@@ -3,14 +3,56 @@ package com.fjbg.thebeercode;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	Button bConnection = null;
+	Button bInscription = null;
+	Button bBeers = null;
+	Button bAdditions = null;
+	Button bMyVotes = null;
+	Button bDisconnection = null;
+	Button bProfile = null;
+	
+	TextView tVWelcome = null;
+	TextView tVNameMenu = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menu_principal_noco);
+		bConnection = (Button)findViewById(R.id.bConnection);
+		bInscription = (Button)findViewById(R.id.bInscription);
+		bBeers = (Button)findViewById(R.id.bBeers);
+		
+		bConnection.setOnClickListener(bConnectionListener);
+		bInscription.setOnClickListener(bInscriptionListener);
+		bBeers.setOnClickListener(bBeersListener);	
 	}
+	
+	private OnClickListener bConnectionListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			
+		}
+	};
+	
+	private OnClickListener bInscriptionListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			
+		}
+	};
+	
+	private OnClickListener bBeersListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			
+		}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
