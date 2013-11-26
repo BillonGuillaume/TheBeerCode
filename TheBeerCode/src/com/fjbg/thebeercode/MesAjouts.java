8b67f8885a751ff2a9ed2ce4cda10f00b968499d
Al0kar <billon.guillaume@gmail.com>
2013-11-26 09:22:37 +0100

    Correction MesAjouts avec AsyncTask., problème d'accès à UI

--- TheBeerCode/src/com/fjbg/thebeercode/MesAjouts.java
+++ TheBeerCode/src/com/fjbg/thebeercode/MesAjouts.java
@@ -2,24 +2,19 @@ package com.fjbg.thebeercode;
 
 import java.util.ArrayList;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.ListActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
-import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.AbsListView.OnScrollListener;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.BaseAdapter;
 
 public class MesAjouts extends Activity {
 	
@@ -27,6 +22,7 @@ public class MesAjouts extends Activity {
 	int items;
 	ArrayAdapter<String> aa;
 	Button bBack;
+	ArrayList<String> aL;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,28 +33,25 @@ public class MesAjouts extends Activity {
 		bBack.setOnClickListener(bBackListener);
 		
 		lvItems = (ListView)findViewById(R.id.lvItems);
-		ArrayList<String> aL = new ArrayList<String>();
-		aL.add("Bière 1");
-		aL.add("Bière 2");
-		aL.add("Bière 3");
-		items = 3;
-		Log.d("MesAjouts", "items ajoutés");
-		aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, aL);
-		lvItems.setAdapter(aa);
-		lvItems.setOnScrollListener(new EndlessScrollListener() {
-			@Override
-			public void onLoadMore(int page, int totalItemsCount) {
-				loadMore(totalItemsCount);
-			}
-		});
+		aL = new ArrayList<String>();		
+		GetBeers getter = new GetBeers();		
+		getter.execute();		
 		
-		lvItems.setOnItemClickListener(new OnItemClickListener()
-		{
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
-				String selectedItem=(String)arg0.getItemAtPosition(arg2);
-			}
-		});
+//		lvItems.setAdapter(aa);
+//		lvItems.setOnScrollListener(new EndlessScrollListener() {
+//			@Override
+//			public void onLoadMore(int page, int totalItemsCount) {
+//				loadMore(totalItemsCount);
+//			}
+//		});
+//		
+//		lvItems.setOnItemClickListener(new OnItemClickListener()
+//		{
+//			@Override
+//			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
+//				String selectedItem=(String)arg0.getItemAtPosition(arg2);
+//			}
+//		});
 	}
 	
 	private OnClickListener bBackListener = new OnClickListener() {
@@ -76,12 +69,8 @@ public class MesAjouts extends Activity {
 	}
 	
 	public void loadMore(int offset) { // TODO en faire une asynctask
-		int nbrAjout;
-		for(nbrAjout = 0; nbrAjout <=5; nbrAjout++) {
-			addItems("Bière " + (items + 1));
-			items++;
-		}	
-		Toast.makeText(this, "Chargement déclenché", Toast.LENGTH_SHORT).show();
+		GetMoarBeers getMore = new GetMoarBeers();
+		getMore.execute();
 	}
 	
 	private void addItems(String item) {
@@ -89,6 +78,86 @@ public class MesAjouts extends Activity {
             this.aa.add(item);
             this.aa.notifyDataSetChanged();
         }
+	}
+	
+	public class GetBeers extends AsyncTask<String, Integer, Boolean>{
+		Boolean exc = false;
+		Exception ex;
+		
+		public GetBeers() {
+
+		}
+
+		@Override
+		protected Boolean doInBackground(String... arg0) {
+			try {
+				aL.add("Bière 1");
+				aL.add("Bière 2");
+				aL.add("Bière 3");
+				items = 3;
+				aa = new ArrayAdapter<String>(MesAjouts.this, android.R.layout.simple_list_item_1, aL);
+				
+				lvItems.setAdapter(aa);
+				lvItems.setOnScrollListener(new EndlessScrollListener() {
+					@Override
+					public void onLoadMore(int page, int totalItemsCount) {
+						loadMore(totalItemsCount);
+					}
+				});
+				
+				lvItems.setOnItemClickListener(new OnItemClickListener()
+				{
+					@Override
+					public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
+						String selectedItem=(String)arg0.getItemAtPosition(arg2);
+					}
+				});
+			}catch(Exception e) {
+				ex = e;
+				exc = true;
+			}
+			return true;
+		}
+		
+		protected void onPostExecute(Boolean result){
+			super.onPostExecute(result);
+			if(exc) {
+				Toast.makeText(MesAjouts.this, ex.getMessage(), Toast.LENGTH_SHORT ).show();
+			}			
+		}
+	}
+	
+	public class GetMoarBeers extends AsyncTask<String, Integer, Boolean>{
+		Boolean exc = false;
+		Exception ex;
+		
+		public GetMoarBeers() {
+
+		}
+
+		@Override
+		protected Boolean doInBackground(String... arg0) {  // TODO Erreur d'accès à l'UI, à régler
+			try {
+				int nbrAjout;
+				for(nbrAjout = 0; nbrAjout <=5; nbrAjout++) {
+					addItems("Bière " + (items + 1));
+					items++;
+				}				
+			}catch(Exception e) {
+				ex = e;
+				exc = true;
+			}
+			return true;
+		}
+		
+		protected void onPostExecute(Boolean result){
+			super.onPostExecute(result);
+			if(exc) {
+				Toast.makeText(MesAjouts.this, ex.getMessage(), Toast.LENGTH_SHORT ).show();
+			} else {
+				Toast.makeText(MesAjouts.this, "Chargement déclenché", Toast.LENGTH_SHORT).show();
+			}
+		}
 	}
 
 }
