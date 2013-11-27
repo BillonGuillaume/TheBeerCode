17456d517f746cd741e123863317f556a8eaad53
Al0kar <billon.guillaume@gmail.com>
2013-11-27 17:06:47 +0100

    correction MesFavoris

--- TheBeerCode/src/com/fjbg/thebeercode/MesFavoris.java
+++ TheBeerCode/src/com/fjbg/thebeercode/MesFavoris.java
@@ -32,7 +32,7 @@ public class MesFavoris extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_mes_ajouts);
+		setContentView(R.layout.activity_mes_favoris);
 		
 		Intent data = getIntent();
 		user = (PersonneDB)data.getParcelableExtra(MainActivity.PERSONNE);
