b7b33849869bef104f87971f52b0fcc84fa36748
Al0kar <billon.guillaume@gmail.com>
2013-12-01 10:53:21 +0100

    Ajout d'une progressbar lors de la co à la DB et d'un bouton pour quitter l'appli

--- TheBeerCode/src/com/fjbg/thebeercode/MainActivity.java
+++ TheBeerCode/src/com/fjbg/thebeercode/MainActivity.java
@@ -1,14 +1,13 @@
 package com.fjbg.thebeercode;
 
-import java.sql.Connection;
-
-import com.fjbg.thebeercode.model.ConnexionDB;
 import com.fjbg.thebeercode.model.PersonneDB;
+import com.fjbg.thebeercode.myconnections.DBConnection;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -23,8 +22,8 @@ public class MainActivity extends Activity {
 	public final static int PROFILE_REQUEST = 5;
 	public final static String USER = "USER";
 	public final static String PERSONNE = "PERSONNE";
-	static Connection connect;
-	static ConnexionDB connec;
+	ProgressDialog progress;
+	static ConnecDB connec;
 	
 	Button bConnection = null;
 	Button bInscription = null;
@@ -35,7 +34,7 @@ public class MainActivity extends Activity {
 	Button bMyFavorites = null;
 	Button bDisconnection = null;
 	Button bProfile = null;
-	Button bMap = null;  // TODO effacer
+	Button bLeave = null;
 	
 	TextView tVWelcome = null;
 	TextView tVNameMenu = null;
@@ -51,26 +50,25 @@ public class MainActivity extends Activity {
 		bInscription = (Button)findViewById(R.id.bInscription);
 		bSearchBeer = (Button)findViewById(R.id.bSearchBeer);
 		
-		bMap = (Button)findViewById(R.id.bMap);
-		bMap.setOnClickListener(bMapListener);
+		bLeave = (Button)findViewById(R.id.bLeave);
+		bLeave.setOnClickListener(bLeaveListener);
 		
 		bConnection.setOnClickListener(bConnectionListener);
 		bInscription.setOnClickListener(bInscriptionListener);
 		bSearchBeer.setOnClickListener(bSearchBeerListener);
 		
 		try {
-			connec = new ConnexionDB();
+			connec = new ConnecDB();
 			connec.execute();
 		} catch (Exception e) {
 			Toast.makeText(MainActivity.this, "Exception  : " + e.getMessage(), Toast.LENGTH_SHORT).show();
 		}	
 	}
 	
-	private OnClickListener bMapListener = new OnClickListener() {  // TODO a effacer
+	private OnClickListener bLeaveListener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			Intent loginActivity = new  Intent(MainActivity.this,MapActivity.class);
-			startActivity(loginActivity);
+			finish();
 			}
 	};
 	
@@ -181,6 +179,7 @@ public class MainActivity extends Activity {
 				bDisconnection = (Button)findViewById(R.id.bDisconnection);
 				bProfile = (Button)findViewById(R.id.bProfile);
 				bMyFavorites = (Button)findViewById(R.id.bMyFavorites);
+				bLeave = (Button)findViewById(R.id.bLeave);
 				
 				bAddBeer.setOnClickListener(bAddBeerListener);
 				bSearchBeer.setOnClickListener(bSearchBeerListener);
@@ -189,6 +188,7 @@ public class MainActivity extends Activity {
 				bDisconnection.setOnClickListener(bDisconnectionListener);
 				bProfile.setOnClickListener(bProfileListener);
 				bMyFavorites.setOnClickListener(bMyFavoritesListener);
+				bLeave.setOnClickListener(bLeaveListener);
 				
 				tVNameMenu.setText(" " + user.getLogin());
 						
@@ -224,6 +224,45 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public class ConnecDB extends AsyncTask<String, Integer, Boolean>{
+		private DBConnection dbconnect = null;
+		Boolean exc = false;
+		Exception ex;
+		
+		public ConnecDB() {
+
+		}
+		
+		protected void onPreExecute(){
+			progress = new ProgressDialog(MainActivity.this);
+			progress.setMessage("Connexion à internet en cours...");
+			progress.setCancelable(false);
+			progress.show();
+       }
+
+		@Override
+		protected Boolean doInBackground(String... arg0) {
+			dbconnect = DBConnection.getInstance();
+			try{
+				dbconnect.init(dbconnect.getConnection());
+			}
+			catch(Exception e){
+				ex = e;
+				exc = true;
+			}
+			return true;
+		}
+		
+		protected void onPostExecute(Boolean result){
+			super.onPostExecute(result);
+			if(progress.isShowing())
+				progress.dismiss();
+			if(exc) {
+				Toast.makeText(MainActivity.this, ex.getMessage(), Toast.LENGTH_SHORT ).show();
+			} else Toast.makeText(MainActivity.this, "Connexion réussie.", Toast.LENGTH_SHORT ).show();
+		}
 	}
 
 }
